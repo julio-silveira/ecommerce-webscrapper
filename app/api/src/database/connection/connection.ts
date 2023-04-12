4b86dd7b8a36628007103a589645fc2cd2bd541f
julio-silveira <julioqueiroz15@gmail.com>
2023-04-12 18:01:51 -0300

    chore: mongoose installed and added to server.ts

--- app/api/src/database/connection/connection.ts
+++
@@ -1,6 +0,0 @@
-// import { DataSource } from 'typeorm';
-
-// export const AppDataSource = new DataSource({
-//   type: 'mongodb',
-//   'url': process.env.MONGO_URI || 'mongodb://mongodb:27017/lexart'
-// })
--- app/api/src/server.ts
+++ app/api/src/server.ts
@@ -1,9 +1,19 @@
 import 'dotenv/config';
 import app from './app';
+import mongoose, { mongo } from 'mongoose';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/lexart'
+
+mongoose.connect(MONGO_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT} and connect to database`);
+    })
+}
+).catch(()=> {
+    process.exit(0)
+})
+
+
 
