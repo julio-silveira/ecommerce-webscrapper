2db501670cea94b042b3edfdf0fa31e6013fd4c1
julio-silveira <julioqueiroz15@gmail.com>
2023-04-12 22:56:00 -0300

    chore: material ui

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -1,26 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button variant="contained">count is</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
