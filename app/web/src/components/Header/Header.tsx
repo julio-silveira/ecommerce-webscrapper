925b230bab3e27f789d1db38224eb917130683d5
julio-silveira <julioqueiroz15@gmail.com>
2023-04-14 09:07:50 -0300

    feat: header

--- app/web/src/components/Header/Header.tsx
+++ app/web/src/components/Header/Header.tsx
@@ -1,6 +1,21 @@
-import { Box } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Header() {
-  return <Box>Header</Box>
+  return (
+    <Stack
+      component="header"
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      px={2}
+    >
+      <Box
+        component="img"
+        sx={{ width: '300px' }}
+        src="https://dev-lexart-corp-site.pantheonsite.io/wp-content/uploads/2022/12/lex-white.svg"
+      />
+      <Typography variant="h5">Search Engine</Typography>
+    </Stack>
+  )
 }
