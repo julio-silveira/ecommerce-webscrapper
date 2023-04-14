402d33ff17d98c5f6af8f4e3395acd0beb8e2dcc
julio-silveira <julioqueiroz15@gmail.com>
2023-04-14 08:52:57 -0300

    chore: theme

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Container } from '@mui/material'
+import { Container, CssBaseline, ThemeProvider } from '@mui/material'
 import { Header } from './components'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
 import { AppProviders } from './providers'
+import theme from './theme/theme'
 
 function App() {
   return (
     <Container>
-      <AppProviders>
-        <Header />
-        <RouterProvider router={router} />
-      </AppProviders>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppProviders>
+          <Header />
+          <RouterProvider router={router} />
+        </AppProviders>
+      </ThemeProvider>
     </Container>
   )
 }
